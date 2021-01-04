c20273d3c7015f5f286be7da9bacc6e2a7f1e58e
jim phan <phanjim2@hotmail.com>
2021-01-04 08:28:07 -0800

    Finished Testing

--- PalindromeChecker.Testing/TestCheckString.cs
+++ PalindromeChecker.Testing/TestCheckString.cs
@@ -7,17 +7,26 @@ namespace PalindromeChecker.Testing
     public class UnitTest1
     {
         [Fact]
-        public void Test_CheckPalindrome()
+        public void Test_CheckPalindromeTrue()
         {
             var testString1 = "level";
             var testString2 = "   level       ";
-            var testString3 = "level is high";
-            var testString4 = "nEver oR eVeN";
+            var testString3 = "nEver oR eVeN";
 
             Assert.True(CheckString.CheckPalindrome(testString1));
             Assert.True(CheckString.CheckPalindrome(testString2));
+            Assert.True(CheckString.CheckPalindrome(testString3));
+        }
+        [Fact]
+        public void Test_CheckPalindromeFalse()
+        {
+            var testString1 = "leveel"; 
+            var testString2 = "  never ror even";
+            var testString3 = "level is    high";
+
+            Assert.False(CheckString.CheckPalindrome(testString1));
+            Assert.False(CheckString.CheckPalindrome(testString2));
             Assert.False(CheckString.CheckPalindrome(testString3));
-            Assert.True(CheckString.CheckPalindrome(testString4));
         }
     }
 }
