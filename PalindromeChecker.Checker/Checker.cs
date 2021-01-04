28b9761d797f5de80be000338e923c1f3c31c4ed
jim phan <phanjim2@hotmail.com>
2021-01-04 07:53:24 -0800

    Finished checker

---
+++ PalindromeChecker.Checker/Checker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PalindromeChecker.Checker
+{
+    public class Checker
+    {
+        public static bool CheckPalindrome(string sentence)
+        {
+            var modifyString = sentence.Trim().ToLower();
+            var cleanString = string.Concat(modifyString.Where(c => !char.IsWhiteSpace(c)));
+            var charArray = cleanString.ToCharArray();
+            for(int index = 0; index < charArray.Count() / 2; index++)
+            {
+                if(charArray[index] != charArray[charArray.Count() - (index + 1)])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- PalindromeChecker.Checker/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PalindromeChecker.Checker
-{
-    public class Class1
-    {
-    }
-}
